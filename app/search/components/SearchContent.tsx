b8ff21ccbde2354f28623646e933ca534964b463
allworkdone <raj635321@gmail.com>
2023-10-10 22:56:38 +0530

    spotify-clone: Favorites Functionality Part-1 Implimented LikeButton for Liking songs implimented insert and delete operation if user like any song using toast to show sucess or error message to user using aifill and aioutline icons for like button icon warning! fix: using encoding npm tool to fix the warning message poping

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -2,6 +2,7 @@
 
 import { Song } from "@/types";
 import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
 
 interface SearchContentProps {
   songs: Song[];
@@ -33,7 +34,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
           <div className="flex-1">
             <MediaItem onClick={() => {}} data={song} />
           </div>
-          {/* TODO: Add Like Button Here */}
+          <LikeButton songId={song.id}/>
         </div>
       ))}
     </div>
